673fbb146aa55218d47b775383c2febce396edbe
David Hutchison <dhutchis68516@gmail.com>
2020-06-02 04:24:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,9 @@
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Mr. Chocolate', 4, 'male', 'brown', 'mild', 1);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Rowdy', 4, 'male', 'black', 'rowdy', 1);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Tabitha', 3, 'female', 'brown', 'mild', 1);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Seargeant Brown', 6, 'male', 'black', 'militant', 1);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Melissa', 5, 'female', 'brown', 'apathetic', 1);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Grinch', 4, 'male', 'brown', 'hates christmas', 1);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Wendy', 4, 'female', 'brown', 'mild', 1);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (null, 4, 'male', 'brown', 'mild', 1);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES 
+  ('Mr. Chocolate', 4, 'male', 'brown', 'mild', 1),
+  ('Rowdy', 4, 'male', 'black', 'rowdy', 1),
+  ('Tabitha', 3, 'female', 'brown', 'mild', 1),
+  ('Seargeant Brown', 6, 'male', 'black', 'militant', 1),
+  ('Melissa', 5, 'female', 'brown', 'apathetic', 1),
+  ('Grinch', 4, 'male', 'brown', 'hates christmas', 1),
+  ('Wendy', 4, 'female', 'brown', 'mild', 1),
+  (null, 4, 'male', 'brown', 'mild', 1);
