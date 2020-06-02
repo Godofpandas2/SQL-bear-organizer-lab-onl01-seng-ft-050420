60f769e678817f9119878b87aecd01a3aec45ea8
David Hutchison <dhutchis68516@gmail.com>
2020-06-02 04:20:52 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,17 @@
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Mr. Chocolate', 4, 'male', 'brown', 'mild', 1)
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Rowdy', 4, 'male', 'black', 'rowdy', 1)
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Tabitha', 3, 'female', 'brown', 'mild', 1)
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Mr. Chocolate', 4, 'male', 'brown', 'mild', 1);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Rowdy', 4, 'male', 'black', 'rowdy', 1);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Tabitha', 3, 'female', 'brown', 'mild', 1);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Seargeant Brown', 6, 'male', 'black', 'militant', 1)
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Melissa', 5, 'female', 'brown', 'apathetic', 1)
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Grinch', 4, 'male', 'brown', 'hates christmas', 1)
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Wendy', 4, 'female', 'brown', 'mild', 1)
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (NULL, 4, 'male', 'brown', 'mild', 1)
+
+* Mr. Chocolate
+* Rowdy
+* Tabitha
+* Sergeant Brown
+* Melissa
+* Grinch
+* Wendy
+* unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
