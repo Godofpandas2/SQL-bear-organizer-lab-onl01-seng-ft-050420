b8b5c3d1dc232f9eaec30042fdfe993c8192a493
David Hutchison <dhutchis68516@gmail.com>
2020-06-02 04:21:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,11 +7,4 @@ INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Grinch'
 INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Wendy', 4, 'female', 'brown', 'mild', 1)
 INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (NULL, 4, 'male', 'brown', 'mild', 1)
 
-* Mr. Chocolate
-* Rowdy
-* Tabitha
-* Sergeant Brown
-* Melissa
-* Grinch
-* Wendy
-* unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
+
