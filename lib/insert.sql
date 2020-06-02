9ad6e2d6b48a3e66cd8e6c82f2934e8b31cc93d0
David Hutchison <dhutchis68516@gmail.com>
2020-06-02 04:28:44 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES
-  ('Mr. Chocolate', 4, 'male', 'brown', 'mild', 1),
-  ('Rowdy', 4, 'male', 'black', 'rowdy', 1),
-  ('Tabitha', 3, 'female', 'brown', 'mild', 1),
-  ('Seargeant Brown', 6, 'male', 'black', 'militant', 1),
-  ('Melissa', 5, 'female', 'brown', 'apathetic', 1),
-  ('Grinch', 4, 'male', 'brown', 'hates christmas', 1),
-  ('Wendy', 4, 'female', 'brown', 'mild', 1),
-  (null, 4, 'male', 'brown', 'mild', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+("Rowdy", 10, "M", "black", "intense", 1),
+("Tabitha", 6, "F", "dark brown", "Nice", 1),
+("Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+("Melissa", 13, "F", "dark brown", "goofy", 1),
+("Grinch", 2, "M", "Black", "Grinchy", 1),
+("Wendy", 6, "F", "Blue", "naive", 1),
+(null, 20, "M", "black", "aggressive", 0);
