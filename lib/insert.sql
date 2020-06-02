c30fed6e56ef044a64938840e26a6b7c7df463aa
David Hutchison <dhutchis68516@gmail.com>
2020-06-02 04:17:15 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Mr. Chocolate', 4, 'male', 'brown', 'mild', 1)
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Rowdy', 4, 'male', 'black', 'rowdy', 1)
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Tabitha', 3, 'female', 'brown', 'mild', 1)
